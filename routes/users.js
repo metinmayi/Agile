aae065b7b23c6ce66fd6d4393bf13f0618d1dab3
michael-cwm <michaelcwm@hotmail.com>
2022-03-21 12:41:21 +0100

    message

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,33 @@
+require("dotenv").config();
+require("../mongoose.js");
+
 const express = require("express");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+const UsersModel = require("../models/UsersModel.js");
+const { ObjectId } = require("mongodb");
+
 router.post("/login", async (req, res) => {
-	res.send("Pong");
+    res.send("Pong");
+});
+
+/////// REGISTER FUNCTION ////////
+
+router.post("/register", async (req, res) => {
+    console.log("working?");
+    const { username, password } = req.body;
+
+    const newUser = new UsersModel({
+        username,
+        hashedPassword: utils.hashPassword(password),
+    });
+
+    console.log("working?");
+
+    await newUser.save();
+
+    res.sendStatus(200);
 });
 
 module.exports = router;
