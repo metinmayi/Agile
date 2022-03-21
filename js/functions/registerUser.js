00cac83ba2db426eb5d4878c78e8948fe729178a
Metin Ahmad <metinmayi@gmail.com>
2022-03-21 13:26:40 +0100

    Validation and API call for registration

--- js/functions/registerUser.js
+++ js/functions/registerUser.js
@@ -1,11 +1,38 @@
 export function registerUser() {
-	document.getElementById("registerForm");
-	registerForm.addEventListener("submit", async (e) => {
+	const form = document.querySelector(".loginForm");
+	// All of the inputs on the page.
+	const formInputs = document.querySelectorAll("input");
+
+	form.addEventListener("submit", async (e) => {
 		e.preventDefault();
-		const response = await fetch("http://localhost:3000/users/register", {
-			method: "POST",
-		});
-		const result = await response.json();
-		console.log(result);
+		// Checks that password and repeat password are a match.
+		if (formInputs[1].value !== formInputs[2].value) {
+			const errorParagraph = document.createElement("p");
+			errorParagraph.setAttribute("id", "errorParagraph");
+			errorParagraph.append(
+				"Dina lösenord matchade inte varandra. Försök igen"
+			);
+			form.appendChild(errorParagraph);
+			return;
+		}
+
+		// Attempts to register the user
+		try {
+			const response = await fetch("http://localhost:3000/users/register", {
+				method: "POST",
+				body: { Kalle: "Kaviar" },
+			});
+			const successParagraph = document.createElement("p");
+			successParagraph.setAttribute("id", "successParagraph");
+			successParagraph.append(
+				"Din registrering lyckades. Du kommer omdirigeras till Login sidan alldeles strax"
+			);
+			form.appendChild(successParagraph);
+			setTimeout(() => {
+				window.location.href = response.url;
+			}, 3000);
+		} catch (error) {
+			console.log(error);
+		}
 	});
 }
