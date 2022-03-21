f750cee0c7d8d1aff4ff8f103c0d0c31befd1b8a
Metin Ahmad <metinmayi@gmail.com>
2022-03-21 12:44:26 +0100

    Implemented CORS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("./mongoose");
 
 const express = require("express");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/users.js");
@@ -9,6 +10,7 @@ const userRoutes = require("./routes/users.js");
 const { ObjectId } = require("mongodb");
 
 const app = express();
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.static("public"));
