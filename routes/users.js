01dc44b602daf4d5cfb6664fd2d711af8813a9a8
Metin Ahmad <metinmayi@gmail.com>
2022-03-21 13:41:41 +0100

    Registration works

--- routes/users.js
+++ routes/users.js
@@ -7,27 +7,29 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const UsersModel = require("../models/UsersModel.js");
 const { ObjectId } = require("mongodb");
+const utils = require("../utils.js");
 
 router.post("/login", async (req, res) => {
-    res.send("Pong");
+	res.send("Pong");
 });
 
 /////// REGISTER FUNCTION ////////
 
 router.post("/register", async (req, res) => {
-    console.log("working?");
-    const { username, password } = req.body;
+	const { username, password } = req.body;
+	const newUser = new UsersModel({
+		username,
+		hashedPassword: utils.hashPassword(password),
+	});
 
-    const newUser = new UsersModel({
-        username,
-        hashedPassword: utils.hashPassword(password),
-    });
+	try {
+		const bajs = await newUser.save();
+		console.log(bajs);
+	} catch (error) {
+		console.log(error);
+	}
 
-    console.log("working?");
-
-    await newUser.save();
-
-    res.sendStatus(200);
+	res.redirect("http://127.0.0.1:5500/login.html");
 });
 
 router.post("/register", async (req, res) => {
