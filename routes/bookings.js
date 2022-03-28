96486241ee630ed2b7a59f4d8a7b7997ecb4bab5
michael-cwm <michaelcwm@hotmail.com>
2022-03-28 15:24:32 +0200

    added edit page

--- routes/bookings.js
+++ routes/bookings.js
@@ -8,21 +8,21 @@ const BookingModel = require("../models/BookingModel.js");
 
 // Get bookings by username
 router.get("/:username", async (req, res) => {
-	const bookings = await BookingModel.find({ username: req.params.username });
-	res.send(bookings);
+    const bookings = await BookingModel.find({ username: req.params.username });
+    res.send(bookings);
 });
 
 // Create Bookings
 router.post("/book", async (req, res) => {
-	const newBooking = new BookingModel(req.body);
-	await newBooking.save();
-	res.send({ message: "Success" });
+    const newBooking = new BookingModel(req.body);
+    await newBooking.save();
+    res.send({ message: "Success" });
 });
 
 // Cancel Booking
 router.post("/cancel", async (req, res) => {
-	await BookingModel.deleteOne({ _id: req.body.id });
-	res.send({ message: "Success" });
+    await BookingModel.deleteOne({ _id: req.body.id });
+    res.send({ message: "Success" });
 });
 
 module.exports = router;
